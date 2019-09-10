9499ba0184970483bfe65e7d2c32d3401c5de48b
Daniel-805 <54294732+Daniel-805@users.noreply.github.com>
2019-09-10 09:45:06 -0700

    Allan did not make a grandpa "and we liked it!" joke today. What a letdown.

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,12 @@
-//
-// Created by aknight on 8/2/19.
-//
+// Created by Daniel Villicana on 7/18/19. Pushed 9/10/19 //
 
+// import namespace standard and connectivity to Allans github page: //
+#include <iostream>
+#include <csv30>
+
+// OS expects arguments of count,value[array] //
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    // Return 0 from EXIT_SUCCESS confirming program ran without errors //
+    return EXIT_SUCCESS;
+}
